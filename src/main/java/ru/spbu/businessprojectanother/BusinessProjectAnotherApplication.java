3749ad28bd59a2f52d1624d27f40db618f8e462e
Awesone <sidorenivan@mail.ru>
2023-11-30 01:25:28 +0300

    init commit

---
+++ src/main/java/ru/spbu/businessprojectanother/BusinessProjectAnotherApplication.java
@@ -0,0 +1,13 @@
+package ru.spbu.businessprojectanother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessProjectAnotherApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BusinessProjectAnotherApplication.class, args);
+  }
+
+}
