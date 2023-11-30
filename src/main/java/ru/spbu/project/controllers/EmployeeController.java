b9c402b22fd3769945eb2a836eb5cb867d9c3a56
Awesone <sidorenivan@mail.ru>
2023-11-30 18:19:33 +0300

    findByStage method implemented

--- src/main/java/ru/spbu/project/controllers/EmployeeController.java
+++ src/main/java/ru/spbu/project/controllers/EmployeeController.java
@@ -3,10 +3,9 @@ package ru.spbu.project.controllers;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.spbu.project.models.Employee;
+import ru.spbu.project.models.enums.Stage;
 import ru.spbu.project.repositories.EmployeeRepository;
 
 @RestController
@@ -19,9 +18,15 @@ public class EmployeeController {
     this.employeeRepository = employeeRepository;
   }
 
-  @GetMapping("/employees")
+  @GetMapping("/all")
   public ResponseEntity<List<Employee>> getParticipants() {
     List<Employee> employeeList = employeeRepository.findAll();
+    return new ResponseEntity<>(employeeList, HttpStatus.OK);
+  }
+
+  @GetMapping("/byStage")
+  public ResponseEntity<List<Employee>> getAllByStage(@RequestParam Stage stage) {
+    List<Employee> employeeList = employeeRepository.findByStage(stage);
     return new ResponseEntity<>(employeeList, HttpStatus.OK);
   }
 }
