19d1a719648c0a23bc47e5c1bce2aa1014032149
Awesone <sidorenivan@mail.ru>
2023-12-02 16:01:53 +0300

    id added

--- src/main/java/ru/spbu/project/models/ProductionPractice.java
+++ src/main/java/ru/spbu/project/models/ProductionPractice.java
@@ -1,6 +1,7 @@
 package ru.spbu.project.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,5 +13,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductionPractice {
+
+  @Id
+  private Long id;
 
 }
