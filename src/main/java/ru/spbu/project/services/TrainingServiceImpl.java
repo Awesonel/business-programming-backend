27999b3d577b3de7b0cf88ac7c76a540b8f0e3d8
vonkka <vonkka@mail.ru>
2023-12-04 12:09:09 +0300

    Order of methods in training service fixed

--- src/main/java/ru/spbu/project/services/TrainingServiceImpl.java
+++ src/main/java/ru/spbu/project/services/TrainingServiceImpl.java
@@ -115,53 +115,6 @@ public class TrainingServiceImpl implements TrainingService {
     return test.getScorePercent() >= 0.8;
   }
 
-  private void checkActionInPast(LocalDate startDate, LocalDate curDate) throws TimeUpException {
-    if (ChronoUnit.DAYS.between(startDate, curDate) < 0) {
-      throw new TimeUpException("Action in the past");
-    }
-  }
-
-  private void checkTime(Employee employee, LocalDate curDate,
-                         long timeDif, Stage stage)
-          throws TimeUpException, DifferentStageException {
-    long days = ChronoUnit.DAYS.between(employee.getStartTime(), curDate);
-    if (days < 0) {
-      throw new TimeUpException("Action in the past");
-    }
-    if (days > timeDif) {
-      employee.setActive(false);
-      switch (stage) {
-        case WAITING_APPLICATION_TRAINING -> {
-          employee.setStage(Stage.REFUSAL_APPLICATION);
-          employeeRepository.save(employee);
-          throw new TimeUpException(
-              "It is possible to answer application in " + INVITATION_TIME_LIMIT
-                  + " days," + " but " + days + " days were passed");
-        }
-        case PASSES_ENTRANCE_TEST -> {
-          employee.setStage(Stage.FAILED_ENTRANCE_TEST);
-          employeeRepository.save(employee);
-          throw new TimeUpException(
-              "It is possible to pass entrance test in " + ENTRY_TEST_TIME_LIMIT
-                  + " days," + " but " + days + " days were passed");
-        }
-        case STUDYING -> {
-          employee.setStage(Stage.FAILED_STUDYING);
-          employeeRepository.save(employee);
-          throw new TimeUpException("It is possible to study in " + STUDY_TIME_LIMIT
-              + " days," + " but " + days + " days were passed");
-        }
-        case EXAM -> {
-          employee.setStage(Stage.FAILED_EXAM);
-          employeeRepository.save(employee);
-          throw new TimeUpException("It is possible to pass exam in " + EXAM_TIME_LIMIT
-              + " days," + " but " + days + " days were passed");
-        }
-        default -> throw new DifferentStageException("There is no such stage!");
-      }
-    }
-  }
-
   @Override
   public boolean takeModuleTest(Long employeeId, TestDTO moduleTest)
       throws TimeUpException, DifferentStageException, TestTypeException {
@@ -203,79 +156,6 @@ public class TrainingServiceImpl implements TrainingService {
       employeeRepository.save(employee);
     }
     return test.getScorePercent() >= 0.8;
-  }
-
-
-
-  private boolean isModuleTest(TestType testType) {
-    return switch (testType) {
-      case ENTRY, PRACTICE_TASK_1, PRACTICE_TASK_2 -> false;
-      case MODULE_1, MODULE_2, MODULE_3, MODULE_4, MODULE_5, MODULE_6, MODULE_7, MODULE_8 -> true;
-    };
-  }
-
-  private boolean isPracticeTaskTest(TestType testType) {
-    return switch (testType) {
-      case ENTRY, MODULE_1, MODULE_2, MODULE_3, MODULE_4, MODULE_5, MODULE_6, MODULE_7, MODULE_8 ->
-          false;
-      case PRACTICE_TASK_1, PRACTICE_TASK_2 -> true;
-    };
-  }
-
-  public boolean checkProgress(Employee employee) {
-    return (passedTest(employee, TestType.MODULE_1) &&
-        passedTest(employee, TestType.MODULE_2) &&
-        passedTest(employee, TestType.MODULE_3) &&
-        passedTest(employee, TestType.MODULE_4) &&
-        passedTest(employee, TestType.MODULE_5) &&
-        passedTest(employee, TestType.MODULE_6) &&
-        passedTest(employee, TestType.MODULE_7) &&
-        passedTest(employee, TestType.MODULE_8) &&
-        passedTest(employee, TestType.PRACTICE_TASK_1) &&
-        passedTest(employee, TestType.PRACTICE_TASK_2));
-  }
-
-  private boolean passedTest(Employee employee, TestType testType) {
-    List<Test> tests = testRepository.employeeFindTest(employee, testType);
-    for (Test test : tests) {
-      if (test.getScorePercent() >= 0.8) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  @Override
-  public HashMap<Stage, Integer> getFromPeriod(LocalDate startTime, LocalDate endTime) {
-    List<Employee> employees = employeeRepository.findAll();
-    Integer studyingCounter = 0;
-    Integer expectsProductionPracticeCounter = 0;
-    Integer productionPracticeCounter = 0;
-    Integer examCounter = 0;
-    Integer failedCounter = 0;
-    Integer passedCounter = 0;
-
-    for (Employee employee : employees) {
-      if (startTime.isBefore(employee.getStartTime()) && endTime.isAfter(employee.getStartTime())) {
-        switch (employee.getStage()) {
-          case STUDYING -> studyingCounter++;
-          case EXPECTS_PRODUCTION_PRACTICE -> expectsProductionPracticeCounter++;
-          case PRODUCTION_PRACTICE -> productionPracticeCounter++;
-          case EXAM -> examCounter++;
-          case FAILED_EXAM -> failedCounter++;
-          case PASSED_EXAM -> passedCounter++;
-        }
-      }
-    }
-
-    HashMap<Stage, Integer> result = new HashMap<>();
-    result.put(Stage.STUDYING, studyingCounter);
-    result.put(Stage.EXPECTS_PRODUCTION_PRACTICE, expectsProductionPracticeCounter);
-    result.put(Stage.PRODUCTION_PRACTICE, productionPracticeCounter);
-    result.put(Stage.EXAM, examCounter);
-    result.put(Stage.FAILED_EXAM, failedCounter);
-    result.put(Stage.PASSED_EXAM, passedCounter);
-    return result;
   }
 
   public void passingProductionPractice(ProductionPracticeDTO productionPracticeDTO)
@@ -347,6 +227,124 @@ public class TrainingServiceImpl implements TrainingService {
       employee.setStage(Stage.PASSED_EXAM);
     }
     return result;
+  }
+
+  @Override
+  public HashMap<Stage, Integer> getFromPeriod(LocalDate startTime, LocalDate endTime) {
+    List<Employee> employees = employeeRepository.findAll();
+    Integer studyingCounter = 0;
+    Integer expectsProductionPracticeCounter = 0;
+    Integer productionPracticeCounter = 0;
+    Integer examCounter = 0;
+    Integer failedCounter = 0;
+    Integer passedCounter = 0;
+
+    for (Employee employee : employees) {
+      if (startTime.isBefore(employee.getStartTime()) && endTime.isAfter(employee.getStartTime())) {
+        switch (employee.getStage()) {
+          case STUDYING -> studyingCounter++;
+          case EXPECTS_PRODUCTION_PRACTICE -> expectsProductionPracticeCounter++;
+          case PRODUCTION_PRACTICE -> productionPracticeCounter++;
+          case EXAM -> examCounter++;
+          case FAILED_EXAM -> failedCounter++;
+          case PASSED_EXAM -> passedCounter++;
+        }
+      }
+    }
+
+    HashMap<Stage, Integer> result = new HashMap<>();
+    result.put(Stage.STUDYING, studyingCounter);
+    result.put(Stage.EXPECTS_PRODUCTION_PRACTICE, expectsProductionPracticeCounter);
+    result.put(Stage.PRODUCTION_PRACTICE, productionPracticeCounter);
+    result.put(Stage.EXAM, examCounter);
+    result.put(Stage.FAILED_EXAM, failedCounter);
+    result.put(Stage.PASSED_EXAM, passedCounter);
+    return result;
+  }
+
+  private void checkActionInPast(LocalDate startDate, LocalDate curDate) throws TimeUpException {
+    if (ChronoUnit.DAYS.between(startDate, curDate) < 0) {
+      throw new TimeUpException("Action in the past");
+    }
+  }
+
+  private void checkTime(Employee employee, LocalDate curDate,
+                         long timeDif, Stage stage)
+          throws TimeUpException, DifferentStageException {
+    long days = ChronoUnit.DAYS.between(employee.getStartTime(), curDate);
+    if (days < 0) {
+      throw new TimeUpException("Action in the past");
+    }
+    if (days > timeDif) {
+      employee.setActive(false);
+      switch (stage) {
+        case WAITING_APPLICATION_TRAINING -> {
+          employee.setStage(Stage.REFUSAL_APPLICATION);
+          employeeRepository.save(employee);
+          throw new TimeUpException(
+                  "It is possible to answer application in " + INVITATION_TIME_LIMIT
+                          + " days," + " but " + days + " days were passed");
+        }
+        case PASSES_ENTRANCE_TEST -> {
+          employee.setStage(Stage.FAILED_ENTRANCE_TEST);
+          employeeRepository.save(employee);
+          throw new TimeUpException(
+                  "It is possible to pass entrance test in " + ENTRY_TEST_TIME_LIMIT
+                          + " days," + " but " + days + " days were passed");
+        }
+        case STUDYING -> {
+          employee.setStage(Stage.FAILED_STUDYING);
+          employeeRepository.save(employee);
+          throw new TimeUpException("It is possible to study in " + STUDY_TIME_LIMIT
+                  + " days," + " but " + days + " days were passed");
+        }
+        case EXAM -> {
+          employee.setStage(Stage.FAILED_EXAM);
+          employeeRepository.save(employee);
+          throw new TimeUpException("It is possible to pass exam in " + EXAM_TIME_LIMIT
+                  + " days," + " but " + days + " days were passed");
+        }
+        default -> throw new DifferentStageException("There is no such stage!");
+      }
+    }
+  }
+
+  private boolean isModuleTest(TestType testType) {
+    return switch (testType) {
+      case ENTRY, PRACTICE_TASK_1, PRACTICE_TASK_2 -> false;
+      case MODULE_1, MODULE_2, MODULE_3, MODULE_4, MODULE_5, MODULE_6, MODULE_7, MODULE_8 -> true;
+    };
+  }
+
+  private boolean isPracticeTaskTest(TestType testType) {
+    return switch (testType) {
+      case ENTRY, MODULE_1, MODULE_2, MODULE_3, MODULE_4, MODULE_5, MODULE_6, MODULE_7, MODULE_8 ->
+              false;
+      case PRACTICE_TASK_1, PRACTICE_TASK_2 -> true;
+    };
+  }
+
+  public boolean checkProgress(Employee employee) {
+    return (passedTest(employee, TestType.MODULE_1) &&
+            passedTest(employee, TestType.MODULE_2) &&
+            passedTest(employee, TestType.MODULE_3) &&
+            passedTest(employee, TestType.MODULE_4) &&
+            passedTest(employee, TestType.MODULE_5) &&
+            passedTest(employee, TestType.MODULE_6) &&
+            passedTest(employee, TestType.MODULE_7) &&
+            passedTest(employee, TestType.MODULE_8) &&
+            passedTest(employee, TestType.PRACTICE_TASK_1) &&
+            passedTest(employee, TestType.PRACTICE_TASK_2));
+  }
+
+  private boolean passedTest(Employee employee, TestType testType) {
+    List<Test> tests = testRepository.employeeFindTest(employee, testType);
+    for (Test test : tests) {
+      if (test.getScorePercent() >= 0.8) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
